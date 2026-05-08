2bec2391ca9705dbd2d6c36f599be52cf40d19c7
Bruna Zuppini <brunazuppini@gmail.com>
2026-05-08 19:49:33 -0300

    Rotas adicionadas!

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,9 +14,12 @@ function Footer() {
                         Acesse minhas redes sociais!
                     </p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold'/>
-                        <InstagramLogoIcon size={48} weight='bold'/>
-                        <FacebookLogoIcon size={48} weight='bold'/>
+                        <a href="https://www.linkedin.com/in/brunazuppini" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold'/>
+                        </a>
+                        <a href="https://www.instagram.com/bruzuppini/" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold'/>
+                        </a>
                     </div>
                 </div>
             </div>
